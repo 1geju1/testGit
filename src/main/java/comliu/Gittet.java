c571a159bbbc1a826fa8e4165070908ad6d897f5
liujianyao <liu@qq.com>
2022-04-22 21:51:03 +0800

    master commit2

--- src/main/java/comliu/Gittet.java
+++ src/main/java/comliu/Gittet.java
@@ -6,5 +6,6 @@ public class Gittet {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git4");
+        System.out.println("master test");
     }
 }
