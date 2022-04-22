4544723fc9e4860a01b2678001eda456cba3f5fb
liujianyao <liu@qq.com>
2022-04-22 21:33:06 +0800

    third commit

--- src/main/java/comliu/Gittet.java
+++ src/main/java/comliu/Gittet.java
@@ -4,5 +4,6 @@ public class Gittet {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("hello git3");
     }
 }
